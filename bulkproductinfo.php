e839711c7268405f4b44e75e3c93040416af6131
SimoneS93 <salernosimone.uni@gmail.com>
2016-02-29 18:06:56 +0100

    more options + performance fix
    update only changed rows, add more editable attributes

--- bulkproductinfo.php
+++ bulkproductinfo.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__.DS.'polyfill.php';
+
 /**
  * Edit products' info in one place
  */
@@ -11,7 +13,7 @@ class BulkProductInfo extends Module
     public function __construct() {
         $this->name = 'bulkproductinfo';
         $this->tab = 'administration';
-        $this->version = '1.0';
+        $this->version = '1.0.1';
         $this->author = 'Simone Salerno';
 
         parent::__construct();
@@ -26,14 +28,24 @@ class BulkProductInfo extends Module
      */
     public function getContent()
     {
-        if (Tools::isSubmit('bulkproductinfo_submit'))
-            $update = $this->updateProductInfo(json_decode(Tools::getValue('bulkproductinfo_data'), true));
+        if (Tools::isSubmit('bulkproductinfo_submit')) {
+            $submit = Tools::getValue('bulkproductinfo_data');
+            $update = $this->updateProductInfo(json_decode($submit));
+        }
         
         Tools::addCSS(__DIR__.DS.'handsontable.full.css');
         Tools::addJS(__DIR__.DS.'handsontable.full.js');
         
-        $productsinfo = $this->getBulkProductsInfo();
-        $this->temp($productsinfo);
+        $productsinfo = $this->castIntToBool(
+            $this->getBulkProductsInfo(),
+            [
+                'available_for_order',
+                'indexed',
+                'active',
+                'show_price',
+                'advanced_stock_management'
+            ]
+        );
         $this->context->smarty->assign('bulkproductsinfo', $productsinfo);
         
         return (isset($update) ? $update : '') . $this->display(__FILE__, 'bulkproductinfo.tpl');
@@ -46,12 +58,14 @@ class BulkProductInfo extends Module
     private function getBulkProductsInfo()
     {
         $query = (new DbQuery)
-                ->select('p.id_product, pl.name')
-                ->select('reference, ean13, upc, minimal_quantity, active')
-                ->select('price, wholesale_price, unity, unit_price_ratio')
-                ->select('width, height, depth, weight')
-                ->select('available_for_order, show_price, `condition`, visibility')
-                ->from('product', 'p')
+                ->select('ps.id_product, pl.name')
+                ->select('p.reference, p.ean13, p.upc, ps.minimal_quantity, ps.active')
+                ->select('ps.price, ps.wholesale_price, ps.unity, ps.unit_price_ratio')
+                ->select('p.width, p.height, p.depth, p.weight')
+                ->select('ps.available_for_order, ps.show_price, ps.`condition`, ps.visibility, ps.indexed')
+                ->select('ps.advanced_stock_management, ps.available_date')
+                ->from('product_shop', 'ps')
+                ->innerJoin('product', 'p', 'p.id_product = ps.id_product')
                 ->innerJoin('product_lang', 'pl', 'pl.id_product = p.id_product')
                 ->where(sprintf(
                         'pl.id_lang = %d AND pl.id_shop = %d', 
@@ -59,53 +73,37 @@ class BulkProductInfo extends Module
                         (int) $this->context->shop->id
                 ))
                 ->orderBy('name');
+        
         return Db::getInstance()->executeS($query);
     }
     
     /**
      * Update product info
-     * @param array $productinfo
+     * @param array $records
      * @return string
      */
-    private function updateProductInfo(array $productinfo)
+    private function updateProductInfo(stdclass $records)
     {
-        $response = '';
-        $temp = $this->temp();
-        //index $temp by id_product, for easy lookup
-        $keys = array_column($temp, 'id_product');
-        $temp = array_combine($keys, $temp);
+        $language = $this->context->language;
+        $shop = $this->context->shop;
+        $response = [];
         
-        //only update changed products
-        foreach ($productinfo as $product) {
-            $id_product = $product['id_product'];
+        foreach ((array) $records as $record) {
+            $message = $this->displayConfirmation(sprintf("Updated %s", $record->name));
             
-            if ($this->diff($product, $temp[$id_product])) {
-                try {
-                    $product_object = new Product($id_product, false, $this->context->language->id);
-                    $this->copy($product_object, $product);
-                    $product_object->save();
-                    $response .= $this->displayConfirmation(sprintf('Updated %s', $product_object->name));
-                }
-                catch (Exception $ex) {
-                    $response .= $this->displayError(sprintf('Error on %s: %s', $product_object->name, $ex->getMessage()));
-                }
+            try {
+                $product = new Product($record->id_product, false, $language->id, $shop->id);
+                $this->fill($product, (array) $record);
+                $product->save();
             }
+            catch (Exception $ex) {
+                $message = $this->displayError(sprintf("Error on %s: [%s]", $record->name, $ex->getMessage()));
+            }
+            
+            array_push($response, $message);
         }
         
-        return $response;
-    }
-    
-    /**
-     * Check if two arrays differ in values
-     * @param array $a
-     * @param array $b
-     */
-    private function diff(array $a, array $b)
-    {
-        sort($a);
-        sort($b);
-        
-        return array_diff($a, $b);
+        return implode('', $response);
     }
     
     /**
@@ -113,7 +111,7 @@ class BulkProductInfo extends Module
      * @param Product $product
      * @param array $data
      */
-    private function copy(Product $product, array $data)
+    private function fill(Product $product, array $data)
     {
         foreach ($data as $key => $value) {
             if (property_exists($product, $key))
@@ -122,17 +120,21 @@ class BulkProductInfo extends Module
     }
     
     /**
-     * Get / set temp data for incremental update
-     * @param array|null $data
-     * @return array|null
+     * Cast 0/1 attributes to true/false
+     * @param array $records
+     * @param array $attributes
+     * @return array
      */
-    private function temp($data = null)
+    private function castIntToBool($records, $attributes)
     {
-        $filename = __DIR__.DS.'temp.json';
-        
-        if ($data !== null)
-            file_put_contents($filename, json_encode($data));
-        else
-            return json_decode(file_get_contents($filename), true);
+        return array_map(function($record) use ($attributes) {
+            foreach ($attributes as $attribute) {
+                if (array_key_exists($attribute, $record)) {
+                    $record[$attribute] = (bool) $record[$attribute];
+                }
+            }
+                
+            return $record;
+        }, $records);
     }
 }
